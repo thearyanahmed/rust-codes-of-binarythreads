e5808bcf0377c0fdab82bc417bc33227e193773a
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-02-05 00:34:26 +0600

    Added binary search

--- src/algorithms/binary_search.rs
+++ src/algorithms/binary_search.rs
@@ -1,30 +1,43 @@
+#![allow(non_snake_case)]
 use rand::{self,Rng};
 
-const MAX : usize = 10000;
+const MAX : usize = 1000000;
 const MAX_VALUE : u32 = 10000000;
 
-pub fn binary_search(array : &mut [u32], target: u32) -> u32 {
-    println!("hello from binary search mod target {}",target);
+pub fn binary_search(array : &mut [i32], target: i32) -> i32 {
+    let mut left = 0;
+    let mut right = array.len() - 1;
 
-    for (_, v) in array.iter().enumerate() {
-        println!("v {} ", v);
+    while left < right {
+        let mid = (left + right) / 2;
+
+        if array[mid] == target {
+            return mid as i32;
+        }
+
+        if target < array[mid] {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
     }
-    return 1;
+
+    return -1;
 }
 
 pub fn run() {
+    let mut ranger = rand::thread_rng();
 
-    let mut rng = rand::thread_rng();
-
-    let mut array: [u32; MAX] = [0; MAX];
+    let mut array: [i32; MAX] = [0; MAX];
 
     for i in 0..MAX {
-        array[i] = rng.gen_range(0..MAX_VALUE);
+        array[i] = ranger.gen_range(0..(MAX_VALUE as i32));
     }
 
-    let val : u32 = 1000000;
+    array.sort();
 
-    let index = binary_search(&mut array,val);
+    let target : i32 = array[ranger.gen_range(0..MAX)];
+    let index = binary_search(&mut array,target);
 
-    println!("found index {} for {}",index, val)
+    println!("array[0] = {} \narray[last] = {}\narray length = {}\ntarget {}\nfound at index {}\n",array[0], array[MAX - 1],array.len(), target,index)
 }
