d7757b23a818b1c9268d347dad68104af7e35f45
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-02-04 22:28:32 +0600

    added fibonacci.rs

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -1,3 +1,34 @@
-pub fn run() {
-    println!("hello from fibonacci");
+#![allow(non_snake_case)]
+
+use std::collections::HashMap;
+
+const FIB_BASE : u64 = 1;
+
+pub fn recursiveFibonacci(n: u64) -> u64 {
+    if n == 0 || n == 1 {
+        return FIB_BASE;
+    }
+
+    return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+}
+
+pub fn optimisedFibonacci(n: u64, map: &mut HashMap<u64,u64>) -> u64 {
+    match n {
+        0 | 1 => FIB_BASE,
+        n => {
+            // if map has a value for key n, return it
+            if map.contains_key(&n) {
+                return *map.get(&n).unwrap();
+            }
+
+            // else figure our the fibonacci value.
+            let v = optimisedFibonacci(n - 1, map) + optimisedFibonacci(n - 2, map);
+
+            // update the map
+            map.insert(n,v);
+
+            // return the new fib. val
+            return v;
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use std::collections::HashMap;
+
 mod fibonacci;
 
 fn main() {
-    fibonacci::run();
+
+    for i in 1..30 {
+        let v = fibonacci::recursiveFibonacci(i);
+
+        println!("{} fib {}",i,v);
+    }
+
+    let mut map = HashMap::new();
+
+    for i in 1..30 {
+        let v = fibonacci::optimisedFibonacci(i, &mut map);
+
+        println!("{} optimised hashmap {}",i, v);
+    }
 }
