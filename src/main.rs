78578923a4b9b1640dda246db49fff0a304aec09
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-02-06 00:18:01 +0600

    Added stack

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
 #![allow(non_snake_case)]
 
-mod ds;
-
 fn main() {
-    let mut q : ds::queue::Queue<isize> = ds::queue::Queue::new();
 
-    q.enqueue(1);
-    let item = q.dequeue();
-    assert_eq!(item, 1);
-    assert_eq!(q.isEmpty(), true);
-    q.peek();
-    println!("length {}",q.length());
 }
 
